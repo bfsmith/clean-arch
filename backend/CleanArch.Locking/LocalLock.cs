41c1f5c0134274ca6978d1d6f12e7edb5a72012d
Blake Smith <howdy2u@gmail.com>
2025-12-07 21:17:23 -0600

    One more bit of cleanup

--- backend/CleanArch.Locking/LocalLock.cs
+++ backend/CleanArch.Locking/LocalLock.cs
@@ -12,29 +12,26 @@ internal class LocalLock : ILock
     public async Task<IDisposable> AcquireAsync()
     {
         await _semaphore.WaitAsync();
-        return new LocalLockHold(_semaphore, () => _semaphore.Release());
+        return new LocalLockHold(() => _semaphore.Release());
     }
 
     private class LocalLockHold : IDisposable
     {
         private bool _disposed;
-        private readonly SemaphoreSlim _semaphore;
         private readonly Action _dispose;
 
         // Takes an action, instead of the semaphore, so there's no chance to change the semaphore's state
         // in some unexpected way.
-        internal LocalLockHold(SemaphoreSlim semaphore, Action dispose)
+        internal LocalLockHold(Action dispose)
         {
-            _semaphore = semaphore;
             _dispose = dispose;
         }
 
         public void Dispose()
         {
-            _semaphore.Release();
-            // ObjectDisposedException.ThrowIf(_disposed, this);
-            // _dispose();
-            // _disposed = true;
+            ObjectDisposedException.ThrowIf(_disposed, this);
+            _dispose();
+            _disposed = true;
         }
     }
 }
